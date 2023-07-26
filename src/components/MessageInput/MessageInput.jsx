8842c1db3b1562dfc7da4af97624e1b384059563
LuisLuna810 <luis.agustin10@hotmail.com>
2023-07-26 12:23:01 -0300

    fix styles Messages

--- src/components/MessageInput/MessageInput.jsx
+++ src/components/MessageInput/MessageInput.jsx
@@ -1,31 +1,42 @@
-import React, { useState } from 'react';
-import { TextField, Button, FormControl, } from '@mui/material';
-import { useMessageContext } from '../context/MessageContext';
+import React, { useState } from "react";
+import { TextField, Button, FormControl, Box, SvgIcon } from "@mui/material";
+import { useMessageContext } from "../context/MessageContext";
+import SendIcon from "@mui/icons-material/Send";
 
 export const MessageInput = ({ doctorId }) => {
-    const [messageContent, setMessageContent] = useState('');
+  const [messageContent, setMessageContent] = useState("");
 
-    const { getMessages, sendMessage } = useMessageContext();
+  const { getMessages, sendMessage } = useMessageContext();
 
-    const handleSubmit = () => {
-        setMessageContent('');
-        sendMessage(messageContent, doctorId)
-        getMessages();
-    }
+  const handleSubmit = () => {
+    setMessageContent("");
+    sendMessage(messageContent, doctorId);
+    getMessages();
+  };
 
-    return (
-        <div>
-            <FormControl variant="outlined">
-                <TextField
-                    label="Type your message"
-                    variant="outlined"
-                    value={messageContent}
-                    onChange={(e) => setMessageContent(e.target.value)}
-                />
-                <Button variant="contained" onClick={handleSubmit}>
-                    Send
-                </Button>
-            </FormControl>
-        </div>
-    );
+  return (
+    <div>
+      <FormControl variant="outlined" sx={{ mt: "10px", width: "100%",p:1 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            gap: 2,
+          }}
+        >
+          <TextField
+            placeholder="Escribir..."
+            sx={{ width: "100%", bgcolor: "white", borderRadius:"10px",}}
+            variant="outlined"
+            value={messageContent}
+            onChange={(e) => setMessageContent(e.target.value)}
+          />
+          <Button variant="contained" onClick={handleSubmit}>
+            <SvgIcon component={SendIcon} inheritViewBox />
+          </Button>
+        </Box>
+      </FormControl>
+    </div>
+  );
 };
--- src/pages/Messages/Messages.jsx
+++ src/pages/Messages/Messages.jsx
@@ -1,18 +1,34 @@
-import React, { useEffect } from 'react';
-import { List, ListItem, ListItemText, Typography, Divider, CircularProgress, Container, } from '@mui/material';
-import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
-import { MessageInput } from '../../components/MessageInput/MessageInput';
-import { useMessageContext } from '../../components/context/MessageContext';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import './Messages.css'
+import React, { useEffect } from "react";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Divider,
+  CircularProgress,
+  Container,
+  Box,
+  SvgIcon,
+  Button,
+} from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import AddIcon from "@mui/icons-material/Add";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import PhoneCallbackIcon from "@mui/icons-material/PhoneCallback";
+import { MessageInput } from "../../components/MessageInput/MessageInput";
+import { useMessageContext } from "../../components/context/MessageContext";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import "./Messages.css";
 
 export const Messages = () => {
-  const { conversations, loading, joinConversation  } = useMessageContext();
+  const { conversations, loading, joinConversation } = useMessageContext();
 
-  useEffect(() => {    
+  const isMobile = useMediaQuery("(max-width:600px)");
+
+  useEffect(() => {
     conversations.forEach((conversation) => {
       joinConversation(conversation.id);
     });
@@ -20,7 +36,14 @@ export const Messages = () => {
 
   if (loading) {
     return (
-      <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100dvh' }}>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100dvh",
+        }}
+      >
         <CircularProgress color="success" />
       </Container>
     );
@@ -28,65 +51,208 @@ export const Messages = () => {
 
   if (conversations.length === 0) {
     return (
-      <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+        }}
+      >
         <p>No tienes conversaciones.</p>
       </Container>
     );
   }
-  console.log(conversations)
+  console.log(conversations);
 
   return (
-    <Container sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', minHeight: '100dvh', minWidth: '99%', margin: 2 }}>
-      <Typography variant='h1' sx={{ color: '#145C6C', fontSize: '3rem' }}>Mis Conversaciones </Typography>
-      <List sx={{ justifySelf: 'center'}}>
-        {conversations.map((conversation) => (
-          <React.Fragment key={conversation.id}>
-            <Accordion sx={{ display: 'flex', flexDirection: 'column', margin: 2, }}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-                sx={{ backgroundColor: 'gray', padding: 4, }}
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "start",
+        minHeight: "100dvh",
+        minWidth: "99%",
+        margin: 2,
+      }}
+    >
+      <Typography
+        variant="h1"
+        sx={{
+          color: "#145C6C",
+          textAlign: isMobile ? "center" : "left",
+          fontFamily: "Work Sans",
+          fontSize: "2.5rem",
+          fontWeight: "700",
+        }}
+      >
+        Chats
+      </Typography>
+      <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
+        <List sx={{ pt: "2rem", width: isMobile ? "50rem" : "95%" }}>
+          {conversations.map((conversation, i) => (
+            <React.Fragment key={conversation.id}>
+              <Accordion
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  margin: 2,
+                  borderRadius: "10px!important",
+                }}
               >
-                <Container id='bardero' sx={{ display: 'flex', gap: 2, justifyContent: 'space-between' }}>
-                  <AddCircleRoundedIcon />
-                  <Typography>{`${conversation.participant2.userType === 'doctor' && 'Dr.' || ''} ${conversation.participant2.name} ${conversation.participant2.lastName}`}</Typography>
-                  <Typography>{`${conversation.participant2.category || 'paciente'}`}</Typography>
-                </Container>
-              </AccordionSummary>
-              <AccordionDetails>
-                <ListItem>
-                  <ListItemText>
-                    <Typography variant="h6">Conversation ID: {conversation.id}</Typography>
-                    <Typography variant="subtitle1">
-                      Participant 1: {conversation.participant1.name}
+                <AccordionSummary
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                  sx={{
+                    backgroundColor: "#838383",
+                    padding: 4,
+                    borderRadius: "10px",
+                  }}
+                >
+                  <Container
+                    id="bardero"
+                    maxWidth={"100%"}
+                    sx={{
+                      display: "flex",
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "left",
+                        flexBasis: "10%",
+                      }}
+                    >
+                      <SvgIcon component={AddIcon} />
+                    </Box>
+                    <Typography sx={{ flexBasis: "20%" }}>
+                      {`Consulta ${i + 1}`}
                     </Typography>
-                    <Typography variant="subtitle1">
-                      Participant 2: {conversation.participant2.name} {conversation.participant2.id}
+                    <Typography sx={{ flexBasis: "20%" }}>
+                      {new Date(
+                        conversation.messages[0].createdAt
+                      ).toLocaleDateString("es-ES")}
                     </Typography>
-                  </ListItemText>
-                </ListItem>
-                <Divider />
-                {conversation.messages
-                  .slice() 
-                  .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
-                  .map((message, i) => (
-                    <ListItem key={i}>
-                      <ListItemText>
-                        <Typography variant="body1">{message.content}</Typography>
-                        <Typography variant="caption">  
-                
-                        </Typography>
-                      </ListItemText>
-                    </ListItem>
-                  ))}
-                <Divider />
-                <MessageInput doctorId={conversation.participant2.id} />
-              </AccordionDetails>
-            </Accordion>
-          </React.Fragment>
-        ))}
-      </List>
+                    <Typography sx={{ flexBasis: "50%" }}>{`${
+                      conversation.participant2.category || "paciente"
+                    }`}</Typography>
+
+                    <SvgIcon component={ExpandMoreIcon} inheritViewBox />
+                  </Container>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Divider />
+                  {conversation.messages
+                    .slice()
+                    .sort(
+                      (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+                    )
+                    .map((message, i) => (
+                      <ListItem key={i}>
+                        <ListItemText
+                          sx={{
+                            display: "flex",
+                            flexDirection:
+                              message.receiver.userType === "doctor"
+                                ? "row-reverse"
+                                : "",
+                          }}
+                        >
+                          <Box
+                            sx={{
+                              display: "flex",
+                              flexDirection: "column",
+                              gap: "5px",
+                              backgroundColor: "rgba(131, 131, 131, 0.22)",
+                              width: "fit-content",
+                              borderRadius: "10px",
+                              padding: "5px",
+                            }}
+                          >
+                            <Typography
+                              sx={{ fontSize: "16px", fontWeight: "700" }}
+                            >
+                              {`${
+                                message.receiver.userType === "doctor"
+                                  ? message.receiver.name
+                                  : message.receiver.name
+                              }:`}
+                            </Typography>
+
+                            <Box
+                              sx={{
+                                display: "flex",
+                                flexDirection: "row",
+                                gap: 1,
+                                p: "1px",
+                                pl: "20px",
+                              }}
+                            >
+                              <Typography
+                                variant="body1"
+                                sx={{ fontSize: "17px", color: "black" }}
+                              >
+                                {`${message.content} `}
+                              </Typography>
+                              <Typography
+                                variant="caption"
+                                sx={{
+                                  fontSize: "12px",
+                                  color: "gray",
+                                  pt: "6px",
+                                }}
+                              >
+                                {`${new Date(
+                                  message.createdAt
+                                ).toLocaleTimeString([], {
+                                  hour: "2-digit",
+                                  minute: "2-digit",
+                                  hour12: false,
+                                })}`}
+                              </Typography>
+                            </Box>
+                          </Box>
+                        </ListItemText>
+                      </ListItem>
+                    ))}
+                  <Box
+                    sx={{
+                      width: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                      bgcolor: "#5F5F5F",
+                      borderRadius: "10px",
+                      p: "5px",
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "flex-end",
+                        p: "5px",
+                      }}
+                    >
+                      <Button>
+                        <SvgIcon component={AttachFileIcon}></SvgIcon>
+                      </Button>
+                      <Button>
+                        <SvgIcon component={PhoneCallbackIcon}></SvgIcon>
+                      </Button>
+
+                      <Typography sx={{ fontSize: "1.5rem", color: "#FFF" }}>
+                        Ir al meet
+                      </Typography>
+                    </Box>
+                    <MessageInput doctorId={conversation.participant2.id} />
+                  </Box>
+                </AccordionDetails>
+              </Accordion>
+            </React.Fragment>
+          ))}
+        </List>
+      </Box>
     </Container>
   );
 };
