4d2151d9cfdfb4d519edb4916a30d073fe762748
aleli <alexredd95@gmail.com>
2023-07-28 19:55:54 -0300

    merge correcto

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { AuthProvider } from "./components/context/AuthContext";
 import { ThemeProvider } from "@emotion/react";
 import { MessageProvider } from './components/context/MessageContext.jsx';
+import { Home } from "./pages/Home/Home";
 import { Header } from "./layout/Header/Header";
 import { Footer } from "./layout/Footer/Footer";
 import { Login } from "./pages/Login/Login";
@@ -62,19 +63,19 @@ function Root() {
             <Header />
             <Routes>
               <Route path="*" element={<Error />} />
-              {/* <Route path="/" element={<Home />} /> */}
+              <Route path="/" element={<Home />} />
               <Route path="/login/*" element={<Login />} />
               <Route path="/register-paciente" element={<RegRoute> <Register /> </RegRoute>} />
               <Route path="/register-doctor" element={<RegRoute> <RegisterDoctor /> </RegRoute>} />
               <Route path="/register-laboratorio" element={<RegRoute> <RegisterLab /> </RegRoute>} />
-              <Route path="/" element={<Especialistas />} />
+              <Route path="/especialistas" element={<Especialistas />} />
               <Route path="/admin/*" element={<AdminRoute> <UserAdmin /> </AdminRoute>} />
               <Route path="/perfil/*" element={<ProfileRoute> <PerfilDoctores /> <PerfilLaboratorios /> <PerfilPacientes /> </ProfileRoute>} />
               <Route path="/consulta/*" element={<Error />} />
               <Route path="/turnos/*" element={<LoginRoute><Turnos location={location} /></LoginRoute>} />
               <Route path="/messages/*" element={<Messages />} />
             </Routes>
-            <Footer />
+            <Footer sx={{mp:"auto" }} />
           </ThemeProvider>
         </MessageProvider>
       </AuthProvider>
--- src/pages/Turnos/Turnos.jsx
+++ src/pages/Turnos/Turnos.jsx
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { Typography, Box, TextField, FormControl, Button } from "@mui/material";
+import {
+  Typography,
+  Box,
+  TextField,
+  FormControl,
+  Button,
+  Container,
+  Avatar,
+} from "@mui/material";
 import axios from "axios";
 import { useAuth } from "../../components/context/AuthContext";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export const Turnos = ({ location }) => {
   const [doctor, setDoctor] = useState([]);
@@ -25,15 +34,16 @@ export const Turnos = ({ location }) => {
     setDoctor(backDoctor.data);
   };
 
-  const addTurno = async (date, userId, doctorId) => {
+  const addTurno = async (date, userId, doctorId, price) => {
     try {
-      console.log(date, userId, doctorId)
+      console.log(date, userId, doctorId);
       const response = await axios.post(
         `${svHost}/turnos`,
         {
           date: date,
           userId,
           doctorId,
+          price
         },
         {
           headers: {
@@ -43,7 +53,7 @@ export const Turnos = ({ location }) => {
       );
 
       if (response.status === 200) {
-        toast.success('Turno sacado con exito!', {
+        toast.success("Turno sacado con exito!", {
           position: "top-right",
           autoClose: 5000,
           hideProgressBar: false,
@@ -52,15 +62,15 @@ export const Turnos = ({ location }) => {
           draggable: true,
           progress: undefined,
           theme: "light",
-        })
+        });
         setTimeout(() => {
-          window.location.href = '/'
-        }, 2000)
+          window.location.href = "/especialistas";
+        }, 2000);
       }
     } catch (error) {
       const status = error.response ? error.response.status : null;
       if (status === 400) {
-        toast.error('El usuario ya tiene turno', {
+        toast.error("El usuario ya tiene turno", {
           position: "top-right",
           autoClose: 5000,
           hideProgressBar: false,
@@ -69,9 +79,9 @@ export const Turnos = ({ location }) => {
           draggable: true,
           progress: undefined,
           theme: "light",
-        })
+        });
       } else {
-        toast.error('Error al sacar turno', {
+        toast.error("Error al sacar turno", {
           position: "top-right",
           autoClose: 5000,
           hideProgressBar: false,
@@ -80,44 +90,199 @@ export const Turnos = ({ location }) => {
           draggable: true,
           progress: undefined,
           theme: "light",
-        })
+        });
       }
     }
   };
-
+  const isMobile = useMediaQuery("(max-width:600px)");
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center', minHeight: '100vh', justifyContent: 'center' }}>
-      <Box component="form" sx={{ display: 'flex', flexDirection: 'column', }}>
-        <Typography variant="h2">Sacar Turno</Typography>
-        <TextField
-          value={`Dr. ${doctor.name} ${doctor.lastName}`}
-          margin="normal"
-          disabled
-          fullWidth
-          id="firstName"
-          label="Doctor"
-          name="doctor"
-          autoComplete="given-name"
-          autoFocus
-        />
-        <TextField
-          value={turno}
-          margin="normal"
-          disabled
-          fullWidth
-          id="turno"
-          label="Fecha y Hora"
-          name="date"
-          autoComplete="family-name"
-        />
-        <Button type="button" variant="contained" color="primary" onClick={() => { addTurno(turno, user.id, doctor.id) }}>
-          Agregar Turno
-        </Button>
-        <Typography sx={{ mt: 4}}>Si la informacion de arriba es incorrecta,</Typography>
-        <Button type="button" variant="contained" color="primary" onClick={() => navigate('/') }>
-          Volver al Inicio
-        </Button>
+    <Container
+      sx={{
+        display: "flex",
+        minHeight: "100vh",
+        width: "100%",
+        justifyContent: "flex-start",
+        flexDirection: "column",
+        gap: 3,
+      }}
+    >
+      <Typography
+        variant="h1"
+        sx={{
+          color: "#145C6C",
+          textAlign: isMobile ? "center" : "left",
+          fontFamily: "Work Sans",
+          fontSize: "2.5rem",
+          fontWeight: "700",
+          pt: 4,
+        }}
+      >
+        Abonar consulta
+      </Typography>
+      <Box
+        component="form"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          bgcolor: "rgba(131, 131, 131, 0.22)",
+          p: 4,
+          width: "100%",
+          borderRadius: 5,
+          gap: 4,
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            color: "#5F5F5F",
+            textAlign: isMobile ? "center" : "left",
+            fontFamily: "Work Sans",
+            fontSize: "2rem",
+            fontWeight: "700",
+          }}
+        >
+          Detalle de la consulta
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 2,
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignContent: "center",
+              textAlign: "center",
+              gap: 2,
+            }}
+          >
+            <Box
+              sx={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignContent: "center",
+              }}
+            >
+              <Avatar
+                alt="img"
+                sx={{
+                  width: "80%",
+                  height: "100%",
+                  justifyContent: "center",
+                  borderRadius: "5rem",
+                }}
+                srcSet={`${doctor.profile_picture_url}`}
+              />
+            </Box>
+
+            <Typography
+              variant="h1"
+              sx={{
+                color: "#fff",
+                textAlign: "center",
+                fontFamily: "Work Sans",
+                fontSize: "1.25rem",
+                fontWeight: "700",
+                bgcolor: "#838383",
+                p: 2,
+                borderRadius: 5,
+                boxSadhow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+              }}
+            >
+              {turno}
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              alignContent: "center",
+              gap: 1,
+            }}
+          >
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <Typography
+                variant="h1"
+                sx={{
+                  color: "#5F5F5F",
+                  textAlign: isMobile ? "center" : "left",
+                  fontFamily: "Work Sans",
+                  fontSize: "1.75rem",
+                  fontWeight: "700",
+                }}
+              >
+                {`Dr. ${doctor.name} ${doctor.lastName}`}
+              </Typography>
+              <Typography
+                variant="h1"
+                sx={{
+                  color: "#5F5F5F",
+                  textAlign: isMobile ? "center" : "left",
+                  fontFamily: "Work Sans",
+                  fontSize: "1.25rem",
+                  fontWeight: "700",
+                }}
+              >
+                {doctor.category}
+              </Typography>
+            </Box>
+            {/*
+            ---------- Luego utilizar esto para ingresar correo y método de pago --------
+            <Box sx={{display:"flex", flexDirection:"column"}}>
+
+            </Box>*/}
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: isMobile? "center":"flex-end",
+            gap: 4,
+          }}
+        >
+          <Typography
+            sx={{
+              color: "#5F5F5F",
+              textAlign: isMobile ? "center" : "right",
+              fontFamily: "Work Sans",
+              fontSize: "1.75rem",
+              fontWeight: "700",
+            }}
+          >{`Total a Pagar: ${doctor.price}${
+            doctor.price ? "USD" : ""
+          }`}</Typography>
+          <Button
+            sx={{ borderRadius: 5, width: "15rem" }}
+            type="button"
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              addTurno(turno, user.id, doctor.id, doctor.price);
+            }}
+          >
+            Pagar
+          </Button>
+          <Button
+            sx={{ borderRadius: 5, width: "15rem" }}
+            type="button"
+            variant="contained"
+            color="primary"
+            onClick={() => navigate("/especialistas")}
+          >
+            Volver a los horarios
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </Container>
   );
 };
