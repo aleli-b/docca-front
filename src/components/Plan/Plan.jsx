1cff0e335284ab755e71bee811f7150792d50017
LuisLuna810 <luis.agustin10@hotmail.com>
2023-08-01 12:26:56 -0300

    subscription

--- src/components/Plan/Plan.jsx
+++ src/components/Plan/Plan.jsx
@@ -1,25 +1,162 @@
-import { Box, Button, Container, Typography } from '@mui/material'
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  Container,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import axios from "axios";
+import { initMercadoPago } from "@mercadopago/sdk-react";
+import { toast } from "react-toastify";
 
 export const Plan = () => {
-    const { userType } = useParams();
-    return (
-        <Container sx={{ display: 'flex', flexDirection: 'column', minHeight: '100dvh', marginTop: 5, alignItems: 'center', gap: 9 }}>
-            <Box>
-                <Typography sx={{ color: '#145C6C', fontWeight: 'bold', }} variant='h2'>Conviertete en un <br />{userType === 'doctor' ? 'doctor' : 'laboratorio'} en línea!</Typography>
-            </Box>
-            <Box>
-                <Typography sx={{ color: '#145C6C' }} variant='h4'>Nuestra plataforma permite operar facilmente con los pacientes y recibir tus honorarios de manera rápida.</Typography>
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
-                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: '#c2c2c2', borderRadius: '10px', }}>
-                    <Typography sx={{ padding: 1, color: '#145C6C', fontWeight: 'bold', fontSize: '2rem' }} variant='body1'>PLAN 1</Typography>
-                    <Typography sx={{ padding: 1, color: '#145C6C', fontWeight: 'bold', fontSize: '1.5rem' }} variant='body1'>Plan integral con una sola especialidad</Typography>
-                    <Typography sx={{ padding: 1, color: '#145C6C', fontWeight: 'bold', fontSize: '1.5rem' }} variant='body1'>PRECIO FINAL: <br /> <span style={{ display: 'flex', justifyContent: 'center' }}> 100USD </span></Typography>
-                    <Button variant='contained' sx={{ marginTop: 4, marginBottom: 1, minWidth: '100%', maxWidth: '100%'}}>Asociarme</Button>
-                </Box>
-            </Box>
-        </Container>
-    )
-}
+  const { user } = useAuth();
+  const isMobile = useMediaQuery("(max-width: 900px)");
+  const { userType } = useParams();
+  const svHost = import.meta.env.VITE_HOST;
+  const mpKey = import.meta.env.VITE_HOST;
+  initMercadoPago(mpKey);
+
+  const createPreference = async (price) => {
+    if (user) {
+      axios
+        .post(`${svHost}/mpcheckoutSubscription`, { user: user, price: price })
+        .then(
+          (response) =>
+            (window.location.href = response.data.response.body.init_point)
+        )
+        .catch((error) => {
+          console.error(error);
+        });
+      console.log(user);
+    } else {
+      toast.warning("Debes iniciar sesión", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
+
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100dvh",
+        marginTop: 5,
+        alignItems: "center",
+        gap: 5,
+      }}
+    >
+      <Box>
+        <Typography
+          sx={{ color: "#145C6C", fontWeight: "bold", textAlign: "center" }}
+          variant={isMobile ? "h3" : "h2"}
+        >
+          Conviertete en un
+        </Typography>
+        <Typography
+          sx={{ color: "#145C6C", fontWeight: "bold", textAlign: "center" }}
+          variant={isMobile ? "h3" : "h2"}
+        >
+          {userType === "doctor" ? "doctor" : "laboratorio"} en línea!
+        </Typography>
+      </Box>
+      <Box>
+        <Typography
+          sx={{ color: "#145C6C", textAlign: "center", fontWeight: "bold" }}
+          variant={isMobile ? "h5" : "h4"}
+        >
+          Nuestra plataforma permite operar facilmente
+        </Typography>
+        <Typography
+          sx={{ color: "#145C6C", textAlign: "center", fontWeight: "bold" }}
+          variant={isMobile ? "h5" : "h4"}
+        >
+          {" "}
+          con los pacientes y recibir tus honorarios de
+        </Typography>
+        <Typography
+          sx={{ color: "#145C6C", textAlign: "center", fontWeight: "bold" }}
+          variant={isMobile ? "h5" : "h4"}
+        >
+          {" "}
+          manera rápida.
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          gap: 5,
+          pb: isMobile ? 5 : "",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            backgroundColor: "#c2c2c2",
+            borderRadius: "10px",
+            width: isMobile ? "90vw" : "28rem",
+            gap: 1,
+          }}
+        >
+          <Typography
+            sx={{
+              color: "#145C6C",
+              fontWeight: "bold",
+              fontSize: "2rem",
+              mt: 2,
+            }}
+            variant="body1"
+          >
+            PLAN 1
+          </Typography>
+          <Typography
+            sx={{ color: "#145C6C", fontWeight: "bold", fontSize: "1.5rem" }}
+            variant="body1"
+          >
+            Plan integral con más
+          </Typography>
+          <Typography
+            sx={{ color: "#145C6C", fontWeight: "bold", fontSize: "1.5rem" }}
+            variant="body1"
+          >
+            {" "}
+            de una especialidad
+          </Typography>
+          <Typography
+            sx={{ color: "#145C6C", fontWeight: "bold", fontSize: "1.5rem" }}
+            variant="body1"
+          >
+            PRECIO FINAL:
+          </Typography>
+          <Typography
+            sx={{ color: "#145C6C", fontWeight: "bold", fontSize: "1.5rem" }}
+          >
+            {" "}
+            999USD{" "}
+          </Typography>
+          <Button
+            variant="contained"
+            onClick={() => createPreference(999)}
+            sx={{ marginTop: 1, minWidth: "100%", maxWidth: "100%" }}
+          >
+            Asociarme
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -1,164 +1,216 @@
-import axios from 'axios';
-import React, { createContext, useContext, useState } from 'react'
-import { toast } from 'react-toastify';
+import axios from "axios";
+import React, { createContext, useContext, useState } from "react";
+import { toast } from "react-toastify";
 
 const AuthContext = createContext();
 
-
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(JSON.parse(localStorage.getItem('user'))) || {};
-    const [token, setToken] = useState(JSON.parse(localStorage.getItem('token'))) || {};
+  const [user, setUser] =
+    useState(JSON.parse(localStorage.getItem("user"))) || {};
+  const [token, setToken] =
+    useState(JSON.parse(localStorage.getItem("token"))) || {};
 
-    const svHost = import.meta.env.VITE_HOST;
+  const svHost = import.meta.env.VITE_HOST;
 
-    const register = async (data) => {
-        try {
-            await axios.post(`${svHost}/users`, data);
-            if (data.userType === 'doctor'){
-                window.location.href = '/plan/doctor';
-            } else if (data.userType === 'lab') {
-                window.location.href = '/plan/lab';
-            } else {
-                const loginData = { email: data.email, password: data.password };
-                toast.success('Creacion de usuario exitosa! Iniciando sesion...', {
-                    position: "top-right",
-                    autoClose: 3000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                });
-                login(loginData);
-            }
-        } catch (error) {
-            if (error.tokenInvalid) logout()
-            toast.error('Ha habido un error, por favor intente mas tarde...', {
-                position: "top-right",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-            });
-        }
+  const register = async (data) => {
+    try {
+      await axios.post(`${svHost}/users`, data);
+        const loginData = {
+          email: data.email,
+          password: data.password,
+          userType: data.userType,
+        };
+        toast.success("Creacion de usuario exitosa!", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+        login(loginData, true);
+
+    } catch (error) {
+      if (error.tokenInvalid) logout();
+      toast.error("Ha habido un error, por favor intente mas tarde...", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     }
+  };
 
-    const login = async (data) => {
+  const login = async (data, register) => {
+    if (register === true) {
         try {
             const loginData = await axios.post(`${svHost}/login`, data);
-            localStorage.setItem('token', JSON.stringify(loginData.data.token));
-            localStorage.setItem('user', JSON.stringify(loginData.data.user));
-            toast.success('Ingreso exitoso! Redirigiendo...', {
-                position: "top-right",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
+            localStorage.setItem("token", JSON.stringify(loginData.data.token));
+            localStorage.setItem("user", JSON.stringify(loginData.data.user));
+            toast.success("Redirigiendo a Planes.", {
+              position: "top-right",
+              autoClose: 3000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
             });
             setTimeout(() => {
-                window.location.href = '/';
-                setUser(loginData.data.user);
-                setToken(loginData.data.token);
-            }, 2000)
-        } catch (error) {
+              window.location.href = `/plan/${data.userType}`;
+              setUser(loginData.data.user);
+              setToken(loginData.data.token);
+            }, 2000);
+          } catch (error) {
             const status = error.response ? error.response.status : null;
-            if (error.tokenInvalid) logout()
+            if (error.tokenInvalid) logout();
             if (status === 404) {
-                toast.error('Usuario o contraseña incorrectos.', {
-                    position: "top-right",
-                    autoClose: 3000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                });
+              toast.error("Usuario o contraseña incorrectos.", {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+              });
             } else {
-                toast.error('Ha habido un error.', {
-                    position: "top-right",
-                    autoClose: 3000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                });
+              toast.error("Ha habido un error.", {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+              });
             }
-        }
-    }
-
-    const editUser = async (data) => {
-        try {
-            await axios.patch(`${svHost}/users/${user.id}`, data, {
-                headers: {
-                    authorization: token,
-                }
-            });
-            toast.success('Usuario editado con éxito, los cambios se efectuarán la próxima vez que inicie sesión', {
-                position: "top-right",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-            });
-        } catch (error) {
-            console.error(error);
-            toast.error('Ha habido un error.', {
-                position: "top-right",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-            });
-        }
-    }
-
-    const forgotPassword = async (email) => {
-        try {
-          const response = await axios.post(`${svHost}/forgot-password`, { email });
-          return response.data.message; // Return the response message (e.g., 'Password reset email sent.')
-        } catch (error) {
-          console.error(error);
-          throw new Error('Failed to send reset password email.'); // You can handle the error as needed
+          }
+    } else {
+      try {
+        const loginData = await axios.post(`${svHost}/login`, data);
+        localStorage.setItem("token", JSON.stringify(loginData.data.token));
+        localStorage.setItem("user", JSON.stringify(loginData.data.user));
+        toast.success("Ingreso exitoso! Redirigiendo...", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+        setTimeout(() => {
+          window.location.href = "/";
+          setUser(loginData.data.user);
+          setToken(loginData.data.token);
+        }, 2000);
+      } catch (error) {
+        const status = error.response ? error.response.status : null;
+        if (error.tokenInvalid) logout();
+        if (status === 404) {
+          toast.error("Usuario o contraseña incorrectos.", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        } else {
+          toast.error("Ha habido un error.", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
         }
       }
-
-    const logout = () => {
-        window.location.href = '/';
-        setUser(null)
-        setToken(null)
-        localStorage.removeItem('token')
-        localStorage.removeItem('user')
     }
+  };
 
-    const auth = {
-        register,
-        login,
-        editUser,
-        logout,
-        forgotPassword,
-        user,
-        token
+  const editUser = async (data) => {
+    try {
+      await axios.patch(`${svHost}/users/${user.id}`, data, {
+        headers: {
+          authorization: token,
+        },
+      });
+      toast.success(
+        "Usuario editado con éxito, los cambios se efectuarán la próxima vez que inicie sesión",
+        {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        }
+      );
+    } catch (error) {
+      console.error(error);
+      toast.error("Ha habido un error.", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     }
+  };
 
-    return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>
-}
+  const forgotPassword = async (email) => {
+    try {
+      const response = await axios.post(`${svHost}/forgot-password`, { email });
+      return response.data.message; // Return the response message (e.g., 'Password reset email sent.')
+    } catch (error) {
+      console.error(error);
+      throw new Error("Failed to send reset password email."); // You can handle the error as needed
+    }
+  };
+
+  const logout = () => {
+    window.location.href = "/";
+    setUser(null);
+    setToken(null);
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  };
+
+  const auth = {
+    register,
+    login,
+    editUser,
+    logout,
+    forgotPassword,
+    user,
+    token,
+  };
+
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+};
 
 export const useAuth = () => {
-    return useContext(AuthContext)
-}
+  return useContext(AuthContext);
+};
